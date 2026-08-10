f17a968600c404e5006f8d63327957b1571182c7
lavipco <mvulam@gmail.com>
2026-08-10 08:25:29 +0700

    10/8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lighting-survey-v98';
+const CACHE = 'lighting-survey-v102';
 
 // Assets tĩnh pre-cache khi install (icon + ảnh mẫu + CAD blocks + title templates T21 + core modules)
 const STATIC_ASSETS = [
